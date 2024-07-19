6d2984bf1da4ec33e20f9a94959b8341e6c3721c
nerella venkata lokeshwar <lokeshwar999@gmail.com>
2024-07-19 17:08:27 +0530

    First commit

---
+++ src/main/java/com/user/user/dao/User.java
@@ -0,0 +1,9 @@
+package com.user.user.dao;
+
+public class User {
+
+	private Long userId;
+	private String userName;
+	private String dept;
+	// private String 
+}
---
+++ src/main/java/com/user/user/interfaces/UserService.java
@@ -0,0 +1,5 @@
+package com.user.user.interfaces;
+
+public class UserService {
+
+}
